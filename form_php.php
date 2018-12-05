3aa4365cfa434f898f5b7b7d0e308edd43244cf2
maxga <maxgal22@wp.pl>
2018-12-05 23:27:10 +0100

    Java Script final

---
+++ form_php.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Formularz</title>
+    <meta name="description" content="Formularz">
+    <meta name="keywords" content="formularz, osobowy">
+    <meta name="author" content="Maksymilian Galbierczyk">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+</head>
+
+<body>
+<h1>Test</h1>
+<?php
+echo "test"
+?>
+</body>
+
+</html>
