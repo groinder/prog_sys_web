9bf9fd751d3638d1bf528198318d7046d3e971f7
maxga <maxgal22@wp.pl>
2018-12-06 00:28:30 +0100

    PHP #1

--- form_php.php
+++ form_php.php
@@ -13,10 +13,47 @@
 </head>
 
 <body>
-<h1>Test</h1>
-<?php
-echo "test"
-?>
+<h1>Znowu formularz</h1>
+
+<form action="wyniki.php" method="post">
+    Imię:<br/>
+    <input type="text" name="imie"/><hr>
+    Nazwisko<br/>
+    <input type="text" name="nazwisko"/><hr>
+    Płeć:<br/>
+    K <input type="radio" name="plec"/>
+    M <input type="radio" name="plec"/>
+    Jeszcze nie wiem <input type="radio" name="plec"/><hr>
+    Nr Telefonu:<br/>
+    <input type="text" name="telefon"/><hr>
+    E-mail<br />
+    <input type="text" name="email" /><hr>
+    Kraj pochodzenia: <br />
+    <select name="country">
+        <option>Polska</option>
+        <option>Niemcy</option>
+        <option>Francja</option>
+        <option>Inny</option>
+    </select><hr>
+    Zainteresowania:<br />
+    <select name="intrests[]" multiple="'multiple">
+        <option>Filmy</option>
+        <option>Książki</option>
+        <option>Sport</option>
+        <option>Muzyka</option>
+    </select><br /><hr>
+    Filmy które widziałem:<br />
+    <input type="checkbox" name="film_1"> Interstellar
+    <input type="checkbox" name="film_2"> Django
+    <input type="checkbox" name="film_3"> Venom
+    <input type="checkbox" name="film_4"> Titanic
+    <input type="checkbox" name="film_5"> Avatar
+    <input type="checkbox" name="film_6"> Gladiator
+    <input type="hidden" name="version" value="formularz 1.0" /><hr>
+    <input type="submit" value="Wyślij">
+</form>
+
+
 </body>
 
 </html>
